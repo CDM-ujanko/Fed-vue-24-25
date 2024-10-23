5658538dc11f143afd2dff71ae72b27956075dd1
Uros Jankovic <uros.jankovic@wundermanthompson.com>
2024-10-23 20:17:14 +0200

    Vue and Vite

---
+++ example-project/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+
+    {{ message  }}
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+
+    <Counter v-for="n in counters"></Counter>
+  </header>
+</template>
+
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+  import Counter from './components/Counter.vue';
+
+  export default {
+    components: {
+      HelloWorld,
+      Counter
+    },
+    data() {
+      return {
+        message: 'Hello world!',
+        counters: 5
+      }
+    }
+  }
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ example-project/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="counter">
+        <p>My internal count is {{ count }}</p>
+        <button @click="count++">Add one</button>
+        <button @click="count--">Reduce one</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Counter',
+        data () {
+            return {
+                count: 0
+            }
+        }
+    }
+</script>
+
+
+<style>
+    .counter {
+        border: 1px solid orchid;
+        border-radius: 30px;
+        padding: 30px;
+        margin: 30px;
+    }
+</style>
